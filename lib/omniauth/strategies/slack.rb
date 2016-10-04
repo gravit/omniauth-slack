67fcc06d0fdcae1531235d80e967b44f95b1cb73
Nanda Yadav <nandayadav@gmail.com>
2016-10-04 11:57:23 -0600

    Make it flexible so identity.basic scope is not required(which cannot be combined with other scopes)

--- lib/omniauth/strategies/slack.rb
+++ lib/omniauth/strategies/slack.rb
@@ -67,16 +67,16 @@ module OmniAuth
       end
 
       def user_identity
-        @user_identity ||= identity['user'].to_hash
+        @user_identity ||= (identity['user'] || {}).to_hash
       end
 
       def team_identity
-        @team_identity ||= identity['team'].to_hash
+        @team_identity ||= (identity['team'] || {}).to_hash
       end
 
       def user_info
         url = URI.parse('/api/users.info')
-        url.query = Rack::Utils.build_query(user: user_identity['id'])
+        url.query = Rack::Utils.build_query(user: user_identity['id'] || access_token.params['user_id'])
         url = url.to_s
 
         @user_info ||= access_token.get(url).parsed
